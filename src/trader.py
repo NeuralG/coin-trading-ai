5de1353520a27481fd991117ee034bdb5cf0fba9
Umutcan Çetin <66206623+NeuralG@users.noreply.github.com>
2026-01-19 21:46:52 +0300

    Add live data

--- src/trader.py
+++ src/trader.py
@@ -1,14 +1,21 @@
 import joblib
 import pandas as pd
+import yfinance as yf
 from fastapi import FastAPI
 from fetch_data import fetch_new_data
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from contextlib import asynccontextmanager
-from config import DATA_FILE_PATH
+from config import DATA_FILE_PATH, TICKER
 from features import add_features
 
 ml_package = {}
 df = pd.DataFrame()
+live_close = None
+
+
+def fetch_live():
+    global live_close
+    live_close = yf.Ticker(TICKER).fast_info["last_price"]
 
 
 def fetch_data_hourly():
@@ -47,9 +54,11 @@ async def lifespan(app: FastAPI):
     global ml_package
     # initial loading
     fetch_data_hourly()
+    fetch_live()
     # fetch data hourly
     scheduler = AsyncIOScheduler()
     scheduler.add_job(fetch_data_hourly, "interval", hours=1)
+    scheduler.add_job(fetch_live, "interval", minutes=1)
     scheduler.start()
     # get the model
     ml_package = get_ml_model("../models/main-model.pkl")
@@ -88,3 +97,24 @@ async def get_prediction():
         "threshold": threshold,
         "action": status,
     }
+
+
+@app.get("/live_price")
+async def get_live_price():
+    return {"price": live_close}
+
+
+@app.get("/chart-data")
+async def get_chart_data():
+    global df
+
+    chart_df = df.tail(200).copy()
+
+    if "Date" not in chart_df.columns:
+        chart_df = chart_df.reset_index()
+
+    clean_data = chart_df[["Date", "Open", "High", "Low", "Close"]]
+
+    clean_data["Date"] = clean_data["Date"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    return clean_data.to_dict(orient="records")
